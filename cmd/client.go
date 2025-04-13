31e8aa35d8fa1596a3ebf8b4237501f9e886549b
jay <maestro1286@gmail.com>
2025-04-13 16:55:17 -0700

    update client command

--- cmd/client.go
+++ cmd/client.go
@@ -1,6 +1,17 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/gomcp/client"
+
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+)
 
 var (
 	clientCmd = &cobra.Command{
@@ -14,6 +25,33 @@ func init() {
 	rootCmd.AddCommand(clientCmd)
 }
 
-func runClientCmd(cmd *cobra.Command, args []string) {
+func getURLS() (*url.URL, *url.URL, error) {
+	sURL := os.Getenv("GOMCP_SERVER_URL")
+	iURL := os.Getenv("GOMCP_INIT_URL")
+	if sURL == "" || iURL == "" {
+		return nil, nil, fmt.Errorf("both GOMCP_SERVER_URL and GOMCP_INIT_URL env vars must be set")
+	}
 
+	serverURL, err := url.Parse(sURL)
+	if err != nil {
+		return nil, nil, err
+	}
+	initURL, err := url.Parse(iURL)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return serverURL, initURL, nil
+}
+
+func runClientCmd(cmd *cobra.Command, args []string) {
+	serverURL, initURL, err := getURLS()
+	if err != nil {
+		log.Fatalf("failed to get server urls: %v", err)
+	}
+
+	client := client.NewMCPClient(serverURL, initURL, uuid.NewString())
+	if err := client.Start(context.Background()); err != nil {
+		log.Fatalf("failed to start mcp client: %v", err)
+	}
 }
