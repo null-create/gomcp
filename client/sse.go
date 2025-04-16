0118cae5aca5688b1275c0f78c7dfad11130b338
jay <maestro1286@gmail.com>
2025-04-15 19:58:08 -0700

    fix error logging

--- client/sse.go
+++ client/sse.go
@@ -1,22 +1,21 @@
 package client
 
 import (
+	"errors"
 	"fmt"
 )
-
-// file for client-side sse handlers
 
 func (c *MCPClient) handleSSE(event, data string) error {
 	switch event {
 	case "endpoint":
 		endpoint, err := c.serverURL.Parse(data)
 		if err != nil {
-			c.log.Error(fmt.Sprintf("error parsing endpoint URL: %v\n", err))
-			return err
+			c.log.Error(fmt.Sprintf("error parsing endpoint URL: %v", err))
+			return fmt.Errorf("error parsing endpoint URL: %v", err)
 		}
 		if endpoint.Host != c.serverURL.Host {
 			c.log.Error("endpoint origin does not match connection origin")
-			return nil
+			return errors.New("endpoint origin does not match connection origin")
 		}
 		c.serverURL = endpoint
 		close(c.endpointChan)
